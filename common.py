a0c9bc164cfd450953a73b69f72fbae33298c08d
Titouan Vial <titouan.vial@grenoble-inp.org>
2024-04-18 13:57:14 +0200

    Suppression lignes inutiles

--- common.py
+++ common.py
@@ -76,7 +76,6 @@ def evil_codemaker(comb_possibles, comb_test):
     # sur la totalité des possibilités restantes avec du backtracking
 
     best_sol = None
-    #best_eval = (0,0)
     # le pire cas est d'avoir supprimé toutes les combinaisons
     best_combs_supprimees = float('inf')
 
@@ -110,7 +109,6 @@ def evil_codemaker(comb_possibles, comb_test):
         if temp_combs_supprimees < best_combs_supprimees:
             best_sol = temp_sol
             best_combs_supprimees = temp_combs_supprimees
-            #best_eval = temp_eval
     
     # on a choisi une solution non optimale car égale à celle que le codebreaker a essayé alors qu'il en existe d'autres
     if best_sol == comb_test and len(comb_possibles) > 1:
