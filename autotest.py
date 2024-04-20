326a76fb6be74bd5a69c3f30ae8165f560277ac6
lougiran <louan.giroud@free.fr>
2024-04-20 12:47:14 +0200

    Finition des tests basiques
    Tests des fonction suivantes :
    - evaluation
    - donner_possible
    - maj_possible
    - evil_codemaker

--- autotest.py
+++ autotest.py
@@ -3,60 +3,91 @@ import common
 # tests sont effectue avec LENGTH = 4  et COLORS = ['R', 'V', 'B', 'J', 'N', 'M', 'O', 'G']
 # sauf en cas de contre indication
 
+
 #%% tests de la fonction evaluation
 
 reference = 'RVBJ'
-test1 = 'RRNM'
-test2 = 'VROJ'
-test3 = 'MMMM'
-test4 = 'RVBJ'
+test = ['RRNM', 'VROJ', 'MMMM', 'RVBJ']
+eval_attendu = [(1,0), (1,2), (0,0), (4,0)]
 
-eval_test1 = common.evaluation(test1, reference)
-if eval_test1 == (1,0):
-    print('test1 est valide')
-    
-eval_test2 = common.evaluation(test2, reference)
-if eval_test2 == (1,2):
-    print('test2 est valide')
-    
-eval_test3 = common.evaluation(test3, reference)
-if eval_test3 == (0,0):
-    print('test3 est valide')
+print(' test de la fonction evaluation \n')
 
-eval_test4 = common.evaluation(test4, reference)
-if eval_test4 == (4,0):
-    print('test4 est valide')
+for i in range(len(test)):
+    evaluation = common.evaluation(reference, test[i])
+    print(f' reference : {reference} \n solution teste : {test[i]} \n evaluation attendu : {eval_attendu[i]} \n evaluation donnee : {evaluation}')
+    if evaluation == eval_attendu[i]:
+        print(" test valide \n")
+    else :
+        print('erreur dans la fonction')
+
 
 
 #%% tests de la fonction donner_possibles
 common.LENGTH = 2
 common.COLORS =["a","b","c"]
 
-eval_donne1 = (1,0)
-comb1= 'aa'
+eval_donne = [(1,0), (0,1)]
+comb= ['aa', "ab"]
+comb_possibles = [{"ab","ac","ba","ca"}, {"bc", "ca"}]
 
-comb_possibles = {"ab","ac","ba","ca"}
-comb1_possibles = common.donner_possibles(comb1, eval_donne1)
 
-if comb1_possibles == comb_possibles:
-    print("test1 de donner_possible valide")
+print('\n test de la fonction donner_possible \n')
+for i in range(len(comb)):
+    comb_possibles_fonction = common.donner_possibles(comb[i], eval_donne[i])
+    print(f' combinaison teste : {comb[i]} \n evaluation donnee : {eval_donne[i]} \n cominaison possibles de l\'evaluation : {comb_possibles[i]} \n combinaison possibles de la fonction : {comb_possibles_fonction}')
+    if comb_possibles_fonction == comb_possibles[i] :
+        print(' test valide \n')
+    else :
+        print("/!\\ la fonction a un probleme")
 
-eval_donne2 = (0,1)
-comb2 = "ab"
-
-comb_possibles = {"bc", "ca"}
-comb2_possibles = common.donner_possibles(comb2, eval_donne2)
-
-if comb_possibles == comb2_possibles:
-    print('test2 de donner_possibles valide')
 common.LENGTH = 4
 common.COLORS = ['R', 'V', 'B', 'J', 'N', 'M', 'O', 'G']
 
 #%% tests de la fonction maj_possibles
 
+common.LENGTH = 3
+common.COLORS =["a","b","c","d"]
+
+comb_possibles = [{"abc", "cba", "aba", "cbc"}, {"abc", "dab", "acc", "cdb", "aaa", "abc"}, {"abd", "abc", "abb"}]
+comb_test = ["abc", "abd", "acd"]
+eval_donnee = [(1,2), (1,0), (1,1)]
+maj_comb_possibles = [{"cba"}, {"acc", "aaa"}, {"abc"}]
+
+print('\n test de la fonction maj_possible \n')
+for i in range(len(comb_possibles)):
+    print(f' combinaison teste : {comb_test[i]} \n evaluation donnee : {eval_donnee[i]} \n ensemble des solutions possibles initialement : {comb_possibles[i]}')
+    common.maj_possibles(comb_possibles[i], comb_test[i], eval_donnee[i])
+    print(f' solution possibles restantes : {maj_comb_possibles[i]} \n solutions possibles de la fonction : {comb_possibles[i]}')
+    if maj_comb_possibles[i] == comb_possibles[i]:
+        print(" test valide \n")
+    else :
+        print("/!\\ la fonction a un probleme")
 
 
+common.LENGTH = 4
+common.COLORS = ['R', 'V', 'B', 'J', 'N', 'M', 'O', 'G']
 
 #%% tests de la fonction evil_codemaker
 
+common.LENGTH = 3
+common.COLORS =["a","b","c","d"]
 
+comb_possibles = [{"aaa","bbb","abc","ccc","cca","acb","bca","bca","abb"}, {"aaa","bbb","abc","ccc","cca","acb","bca","bca","abb"}]
+comb_test = ["abc", "aac"]
+solutions_optimales = [{"aaa","bbb","ccc"}, {"aaa", "ccc", "abb"}]
+
+print('\n test de la fonction evil_codemaker \n')
+
+for i in range(len(comb_test)):
+    print(f' combinaison teste : {comb_test[i]} \n ensemble des solutions possibles initialement : {comb_possibles[i]}')
+    sol = common.evil_codemaker(frozenset(comb_possibles[i]), comb_test[i])
+    print(f' solution idéales possibles : {solutions_optimales[i]} \n solution propose : {sol}')
+    if sol in list(solutions_optimales[i]) :
+        print(" test valide \n")
+    else :
+        print("/!\\ la fonction a un probleme")
+
+
+
+common.LENGTH = 4
+common.COLORS = ['R', 'V', 'B', 'J', 'N', 'M', 'O', 'G']
